5e8450559a7c1b3b6492be652fe4b0a510ee0bb6
tuongtv <vinhtuong.contact@gmail.com>
2022-10-21 16:20:45 +1100

    update first

---
+++ _24Pharse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF
+{
+    internal class _24Pharse
+    {
+        string _12Pharse = "tent snack monkey expect wedding gauge basic sense merge indicate you foil";
+    }
+}
