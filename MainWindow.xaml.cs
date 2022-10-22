90a1782555168452125cdec9d67491bc63371b9e
tuongtv <vinhtuong.contact@gmail.com>
2022-10-22 22:41:42 +1100

    Updated Add Database from DataSeed

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,12 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 
@@ -17,56 +24,96 @@ namespace WPF
             var converter = new BrushConverter();
             ObservableCollection<Member> members = new ObservableCollection<Member>();
 
-            //Create DataGrid Items info
-            members.Add(new Member { Number = "1", Character = "J", BgColor = (Brush)converter.ConvertFromString("#1098ad"), Name = "Tinker Truong", Position = "Slicer", Email = "vinhtuong.contact@gmail.com", Phone = "0455990697" });
-            members.Add(new Member { Number = "2", Character = "J", BgColor = (Brush)converter.ConvertFromString("#1098ad"), Name = "Tinker Truong", Position = "Slicer", Email = "vinhtuong.contact@gmail.com", Phone = "0455990697" });
-            members.Add(new Member { Number = "3", Character = "J", BgColor = (Brush)converter.ConvertFromString("#1098ad"), Name = "Tinker Truong", Position = "Slicer", Email = "vinhtuong.contact@gmail.com", Phone = "0455990697" });
-            members.Add(new Member { Number = "4", Character = "J", BgColor = (Brush)converter.ConvertFromString("#1098ad"), Name = "Tinker Truong", Position = "Slicer", Email = "vinhtuong.contact@gmail.com", Phone = "0455990697" });
-            members.Add(new Member { Number = "5", Character = "J", BgColor = (Brush)converter.ConvertFromString("#1098ad"), Name = "Tinker Truong", Position = "Slicer", Email = "vinhtuong.contact@gmail.com", Phone = "0455990697" });
-            members.Add(new Member { Number = "6", Character = "J", BgColor = (Brush)converter.ConvertFromString("#1098ad"), Name = "Tinker Truong123123123", Position = "Slicer", Email = "vinhtuong.contact@gmail.com", Phone = "0455990697" });
-            members.Add(new Member { Number = "7", Character = "J", BgColor = (Brush)converter.ConvertFromString("#1098ad"), Name = "Tinker Truong", Position = "Slicer", Email = "vinhtuong.contact@gmail.com", Phone = "0455990697" });
-            members.Add(new Member { Number = "8", Character = "J", BgColor = (Brush)converter.ConvertFromString("#1098ad"), Name = "Tinker Truong", Position = "Slicer", Email = "vinhtuong.contact@gmail.com", Phone = "0455990697" });
-            members.Add(new Member { Number = "9", Character = "J", BgColor = (Brush)converter.ConvertFromString("#1098ad"), Name = "Tinker Truong", Position = "Slicer", Email = "vinhtuong.contact@gmail.com", Phone = "0455990697" });
-            members.Add(new Member { Number = "10", Character = "J", BgColor = (Brush)converter.ConvertFromString("#1098ad"), Name = "Tinker Truong", Position = "Slicer", Email = "vinhtuong.contact@gmail.com", Phone = "0455990697" });
-            members.Add(new Member { Number = "11", Character = "J", BgColor = (Brush)converter.ConvertFromString("#1098ad"), Name = "Tinker Truong", Position = "Slicer", Email = "vinhtuong.contact@gmail.com", Phone = "0455990697" });
-            members.Add(new Member { Number = "12", Character = "J", BgColor = (Brush)converter.ConvertFromString("#1098ad"), Name = "Tinker Truong", Position = "Slicer", Email = "vinhtuong.contact@gmail.com", Phone = "0455990697" });
+            var membersData = File.ReadAllText("D:/Dev/CSharp/Learn/CSharp/HKTeam/WPF//SeedData/members.json");
+            var memberList = JsonSerializer.Deserialize<List<Member>>(membersData);
+            foreach (var item in memberList)
+            {
+                members.Add(item);
+            }
 
             membersDataGrid.ItemsSource = members;
         }
+        private static Random random = new Random();
 
-        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        public static string RandomString(int length)
         {
-            if (e.ChangedButton == MouseButton.Left)
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+        private void DataGrid_MouseLeftButtonDoubleClick(object sender, RoutedEventArgs e)
+        {
+            try
             {
-                this.DragMove();
+                if (sender != null)
+                {
+                    DataGrid grid = sender as DataGrid;
+                    if (grid != null && grid.SelectedItems != null && grid.SelectedItems.Count == 1)
+                    {
+                        //This is the code which helps to show the data when the row is double clicked.
+                        //DataGridRow dgr = grid.ItemContainerGenerator.ContainerFromItem(grid.SelectedItem) as DataGridRow;
+                        //DataRowView dr = (DataRowView)dgr.Item;
+
+                        //String ProductName = dr[1].ToString();
+                        //String ProductDescription = dr[2].ToString();
+                        MessageBox.Show("You Clicked : \r\nName : " +  "\r\nDescription : ");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
             }
         }
 
         private bool IsMaximized = false;
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                //if (IsMaximized)
+                //{
+                //    this.WindowState = WindowState.Normal;
+                //    this.Height = 720;
+                //    this.Width = 1080;
+
+                //    IsMaximized = false;
+                //}
+                this.DragMove();
+            }
+        }
+
         private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ClickCount == 2)
             {
-                //this.WindowState = WindowState.Minimized;
-                this.Width = 1080;
-                this.Height = 720;
+                if (IsMaximized)
+                {
+                    this.WindowState = WindowState.Normal;
+                    this.Height = 720;
+                    this.Width = 1080;
 
-                IsMaximized = false;
-            } 
-            else
-            {
-                this.WindowState = WindowState.Maximized;
+                    IsMaximized = false;
+                }
+                else
+                {
+                    this.WindowState=WindowState.Maximized;
 
-                IsMaximized = true;
+                    IsMaximized = true;
+                }
             }
         }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
-    
+
     public class Member
     {
         public string Character { get; set; }
-        public string Number { get; set; }
+        public int Number { get; set; }
         public string Name { get; set; }
         public string Position { get; set; }
         public string Email { get; set; }
